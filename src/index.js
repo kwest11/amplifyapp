3fd50da02f7517aa6372d4d4eb78a5039c0353e4
kwest11 <kwest11@gmail.com>
2021-11-01 20:49:01 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <!-- <App /> -->
     <Timer />
   </React.StrictMode>,
   document.getElementById('root')

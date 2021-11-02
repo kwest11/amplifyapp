e20c6b81fcd4b60f5bf43cfd4ae2f35c8a8d524c
kody <kwest11@gmail.com>
2021-11-02 12:17:22 -0400

    Modified image sources

--- src/Whitegrass.js
+++ src/Whitegrass.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 class Whitegrass extends React.Component {
+	static img1_src = "https://whitegrass.com/video.jpg?t=";
+	static img2_src = "https://whitegrass.com/video.jpg1?t=";
+	
     constructor(props) {
       super(props);
       this.state = { date: 0 };
@@ -24,12 +27,14 @@ class Whitegrass extends React.Component {
       return (
         <div>
 		  Date: {this.state.date}
-		  https://whitegrass.com/video1.jpg?t={this.state.date}
+		  <h3>Gandalfs Webcam</h3>
+		  <h3>{this.img1_src}+{this.state.date}</h3>
 		  <p>
-		  	<img src="https://whitegrass.com/video.jpg?t={this.state.date}" id="img1" />
+		  	<img src="{this.img1_src}+{this.state.date}" id="img1" />
 		  </p>
+		  <h3> Weiss Knob Slope Webcam </h3>
 		  <p>
-			<img src="https://whitegrass.com/video1.jpg?t={this.state.date}" width="1280" height="720" id="img2" />
+			<img src="{this.img2_src}+{this.state.date}" width="1280" height="720" id="img2" />
 		  </p>
         </div>
       );

fcd5ae9308e2026a4615c9e4a5b3ed5a229ad7f3
kody <kwest11@gmail.com>
2021-11-02 10:57:42 -0400

    Moved Timer into index and out of App

---
+++ src/Whitegrass.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Whitegrass extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { date: 0 };
+    }
+	
+    tick() {
+      this.setState(state => ({
+		  date: new Date().getTime();
+      }));
+    }
+
+    componentDidMount() {
+      this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.interval);
+    }
+	
+    render() {
+      return (
+        <div>
+		  Date: {this.state.date}
+		  https://whitegrass.com/video1.jpg?t=
+        </div>
+      );
+    }
+}
+
+export default Whitegrass;

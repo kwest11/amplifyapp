97d544a1cc84bc69d64d6105637d68d191af71c7
kody <kwest11@gmail.com>
2021-11-02 21:00:31 -0400

    Added image slider

--- src/Whitegrass.js
+++ src/Whitegrass.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 class Whitegrass extends React.Component {
 	
+	static images = [
+	  { url: "https://whitegrass.com/video.jpg?t=0" },
+	  { url: "https://whitegrass.com/video1.jpg?t=" }
+	];
+	
     constructor(props) {
       super(props);
       this.state = { date: 0 };
-	  this.props = { img1_src: 'https://whitegrass.com/video.jpg?t=', img2_src: 'https://whitegrass.com/video1.jpg?t=' }
+	  this.props = { img2_src: 'https://whitegrass.com/video1.jpg?t=' , img1_src : 'https://whitegrass.com/video.jpg?t='}
     }
 	
     tick() {
@@ -24,18 +29,19 @@ class Whitegrass extends React.Component {
     }
 	
     render() {
+	  
       return (
         <div>
 		  Date: {this.state.date}
 		  <h3>Gandalfs Webcam</h3>
-		  <h3>{this.props.img1_src}+{this.state.date}</h3>
+		  <h3>{this.props.img1_src}</h3>
 		  <p>
-		  	<img src="{this.props.img1_src}+{this.state.date}" id="img1" />
+		  	<img src={this.props.img1_src} id="img1" />
 		  </p>
 		  <h3> Weiss Knob Slope Webcam </h3>
 		  <h3>{this.props.img2_src}</h3>
 		  <p>
-			<img src="{this.props.img2_src}" width="1280" height="720" id="img2" />
+			<img src={this.props.img2_src} width="1280" height="720" id="img2" />
 		  </p>
         </div>
       );
--- src/index.js
+++ src/index.js
@@ -2,15 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 import Timer from './Timer';
 import Whitegrass from './Whitegrass';
-import reportWebVitals from './reportWebVitals';
+import SimpleImageSlider from "react-simple-image-slider";
+
+const images = [
+  { url: "https://whitegrass.com/video.jpg?t=0" },
+  { url: "https://whitegrass.com/video1.jpg?t=" }
+];
 
 ReactDOM.render(
   <React.StrictMode>
 	<>
 	  	<h3>Kodys Test</h3>
 		<App />
+		<SimpleImageSlider
+        	width={896}
+        	height={504}
+        	images={images}
+        	showBullets={true}
+        	showNavs={true}
+      	  />
 		<Whitegrass />
 	</>
   </React.StrictMode>,

9fc6f6e6b37c3769af303fbf5d881d62617ed298
kody <kwest11@gmail.com>
2021-11-02 22:07:56 -0400

    Added hls player for canaan webcams

--- src/CanaanWebcams.js
+++ src/CanaanWebcams.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import ClapprPlayer from './clappr-player.jsx';
+import ReactHlsPlayer from 'react-hls-player';
 
 class CanaanWebcams extends React.Component {
   render() {
     return <div>
       <h1>Canaan Webcams</h1>
-      <ClapprPlayer source="https://55507e0501d25.streamlock.net/live/canaanresortbase.stream/playlist.m3u8" />
+  <ReactHlsPlayer
+      src="https://55507e0501d25.streamlock.net/live/canaanresortbase.stream/playlist.m3u8"
+      autoPlay={true}
+      controls={false}
+      width="100%"
+      height="auto"
+    />
     </div>;
   }
 }

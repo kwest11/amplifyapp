da2d66fbf2d0cacb9b24818f608ee3c03c4ed7ea
kody <kwest11@gmail.com>
2023-11-30 08:42:58 -0500

    Removed apostrophes

--- src/About.js
+++ src/About.js
@@ -8,11 +8,11 @@ export function About() {
 
         <h1>About Me</h1>
 
-        <p>Welcome to my personal website! I'm MountyKoder, and this is my little corner of the internet where I aggregate various feeds of information that interest me.</p>
+        <p>Welcome to my personal website! I am MountyKoder, and this is my little corner of the internet where I aggregate various feeds of information that interest me.</p>
 
-        <p>Behind the scenes, I curate content from different sources to create a personalized experience. While most of the website is protected behind a username and password, I've decided to share this about page to give you a glimpse into who I am and what you can expect to find here.</p>
+        <p>Behind the scenes, I curate content from different sources to create a personalized experience. While most of the website is protected behind a username and password, I have decided to share this about page to give you a glimpse into who I am and what you can expect to find here.</p>
 
-        <p>I'm passionate about West Virginia, skiing, and running. This website reflects my ongoing exploration of these subjects. Feel free to explore and get a sense of what makes this space uniquely mine.</p>
+        <p>I am passionate about West Virginia, skiing, and running. This website reflects my ongoing exploration of these subjects. Feel free to explore and get a sense of what makes this space uniquely mine.</p>
 
       </body>
     </div>

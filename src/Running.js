f1c78d45c04f0c98fbcf05d1a4761038ad7446a7
kody <kwest11@gmail.com>
2023-12-02 19:02:22 -0500

    changed grafana url

--- src/Running.js
+++ src/Running.js
@@ -18,11 +18,9 @@ class Running extends React.Component {
                         <iframe minHeight="160px" width='100%' height="30%" frameBorder='0' allowtransparency='true' scrolling='no' src='https://www.strava.com/athletes/27660355/activity-summary/61367fa622b91661dbe894320521bdfe7e0f0529'></iframe>
                         {/* Strava - Activities */}
                         <iframe minHeight="454" width='100%' height='70%' frameBorder='0' allowtransparency='true' scrolling='no' src='https://www.strava.com/athletes/27660355/latest-rides/61367fa622b91661dbe894320521bdfe7e0f0529'></iframe>
-                        {/* Grafan - Health Dashboard 
+                        {/* Grafana - Health Dashboard */}
                         <iframe minHeight="454" width='100%' height='70%' frameBorder='0' allowtransparency='true' scrolling='no' src='https://mountykoder.grafana.net/public-dashboards/c31c6848e21145d0bcc76f312759c1b6'></iframe>
-    */}
-
-                        <img src="https://mountykoder.grafana.net/d/ce8d9fd5-b9c4-4d13-a317-b6f9ee15d980/health-dashboard?orgId=1&from=1672549200000&to=1704085199999"></img>
+                        {/* <img src="https://mountykoder.grafana.net/d/ce8d9fd5-b9c4-4d13-a317-b6f9ee15d980/health-dashboard?orgId=1&from=1672549200000&to=1704085199999"></img> */}
                     </View>
                 </Grid>
             </View>
